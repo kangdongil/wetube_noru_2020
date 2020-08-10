47335222bb05cb05b4b753b207a822bb63439bb4
kangdongil <67248006+kangdongil@users.noreply.github.com>
2020-08-11 00:29:49 +0900

    Middleware

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-const express = require("express");
+// const express = require("express");
+import express from "express";
 const app = express();
-
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const PORT = 4000;
 
-function handleListening() {
-    console.log(`Listening on: http://locahost:${PORT}`);
-}
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-app.listen(4000, handleListening);
+const handleHome = (req, res) => res.send("Hello from home");
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+const betweenHome = (req, res, next) => {
+    console.log("I'm between");
+    next();
+};
+
+//Execute Process
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/profile", handleProfile);
+
+// app.get("/", betweenHome, handleHome);
+app.use(betweenHome);
+
+app.listen(PORT, handleListening);
