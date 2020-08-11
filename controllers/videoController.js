9f8cff97f8c4145edb42d934f91b9c1ea969bd84
kangdongil <67248006+kangdongil@users.noreply.github.com>
2020-08-11 13:17:43 +0900

    HTML works

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+
 export const search = (req, res) => {
     // const searchingBy = req.query.term;
     const {
@@ -6,7 +7,11 @@ export const search = (req, res) => {
     } = req;
     res.render("search", { pageTitle: "Search", searchingBy });
 };
+
 export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
+
 export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
+
 export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
